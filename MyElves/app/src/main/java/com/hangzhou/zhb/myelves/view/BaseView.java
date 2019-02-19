99f80cdc2cce66f3a11321b8ef3c01fd4f79246f
zzhbing <389548545@qq.com>
2019-02-19 20:31:05 +0800

    Signed-off-by: zzhbing <389548545@qq.com>

---
+++ MyElves/app/src/main/java/com/hangzhou/zhb/myelves/view/BaseView.java
@@ -0,0 +1,39 @@
+package com.hangzhou.zhb.myelves.view;
+
+import android.content.Context;
+import android.graphics.Paint;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.hangzhou.zhb.myelves.R;
+
+/**
+ * Created by zhb on 2018/12/10
+ * 作用：
+ */
+public class BaseView extends View {
+    Paint paint = new Paint();
+
+    public BaseView(Context context) {
+        super(context);
+    }
+
+    public BaseView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BaseView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public BaseView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    private void initPaint(){
+        paint.setColor(getResources().getColor(R.color.B_00FFFF));
+    }
+}
